8ced7fdb2f1d74e40cc8fc29267b65a16e014e77
yancao <cao.yan@southgis.com>
2020-08-26 19:35:18 +0800

    第一次提交

---
+++ src/main/java/com/yc/articlemongon/ArticlemongonApplication.java
@@ -0,0 +1,13 @@
+package com.yc.articlemongon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlemongonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticlemongonApplication.class, args);
+	}
+
+}
